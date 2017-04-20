56d14a07e02f655094a0245fe2796dfba7de42ed
Trigex <trigexss@gmail.com>
2017-04-20 16:08:19 -0700

    update, notifications, and api

--- public/js/textchan.js
+++ public/js/textchan.js
@@ -1,9 +1,36 @@
-var clicked = false;
+/* Textchan
+ * Licensed under the MIT License
+ * (c) 2017 Trigex
+ */
 
-$("#refresh").click(function(){
-    if(!clicked) {
-        clicked = true;
-    } else {
-        clicked = false;
-    }
-});
+if($(".thread").length) {
+    var clicked = false;
+    var threadId = $("input:hidden").val();
+    var posts = $(".post").length;
+
+    $("#refresh").click(function(){
+        if(!clicked) {
+            clicked = true;
+        } else {
+            clicked = false;
+        }
+    });
+
+    setInterval(function(){
+        if(clicked) {
+            $(".thread").load("/api/thread/" + threadId);
+            var newPosts = $(".post").length;
+            if(newPosts > posts) {
+                posts = newPosts;
+                Push.create("Textchan", {
+                    body: "There were new posts!",
+                    timeout: 5000,
+                    onClick: function () {
+                        window.focus();
+                        this.close();
+                    }
+                });
+            }
+        }
+    }, 5000);
+}
